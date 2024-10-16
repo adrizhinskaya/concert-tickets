ab2962061dec417445ad22ddd66b0ae535bdf380
Alina <adrizhinskaya@bk.ru>
2024-10-16 19:30:19 +0300

    fix: fix pr 2

--- src/model/Printable.java
+++ src/model/Printable.java
@@ -1,8 +1,6 @@
 package model;
 
 public interface Printable {
-    void printRole();
-
     default void print() {
         System.out.println("print content in console");
     }
--- src/model/enums/User.java
+++ src/model/enums/User.java
@@ -6,4 +6,9 @@ public class User extends EntityWithId {
     public User() {
         super();
     }
+
+    public void printRole() {
+        System.out.println("USER printRole() method");
+    }
+
 }
